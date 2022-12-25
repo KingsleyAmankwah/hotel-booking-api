1d970243fd59893b14c68df91d254aa23c2d16fa
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-25 06:09:54 +0000

    App entry file

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,23 @@ const dotenv = require("dotenv").config();
 const colors = require("colors");
 const connectDB = require("./config/db");
 const { errorHandler } = require("./middlewares/errorMiddleware");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 
-const Port = process.env.PORT || 8000;
+const Port = process.env.PORT;
+
+//middlewaeres
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/auth", require("./routes/authRoute"));
+app.use("/api/users", require("./routes/userRoute"));
+// app.use("/api/hotels", require("./routes/hotelRoute"));
+// app.use("/api/rooms", require("./routes/roomsRoute"));
 
 app.use(errorHandler);
 
