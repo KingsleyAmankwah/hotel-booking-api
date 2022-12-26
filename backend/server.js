e93b675cf498c6e36bdded5625b9aba9e691cc06
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-26 20:26:31 +0000

    cors credentials added

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv().config();
 const Port = process.env.PORT;
 
 //middlewaeres
-app.use(cors());
+app.use(cors({ credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
