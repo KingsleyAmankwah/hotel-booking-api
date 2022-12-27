5191c265324698b6cf1465d52676dd320def30da
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-27 05:16:29 +0000

    app entry file

--- backend/server.js
+++ backend/server.js
@@ -7,19 +7,18 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 const app = express();
-dotenv().config();
+dotenv.config();
 
 const Port = process.env.PORT;
 
 //middlewaeres
-app.use(cors({ credentials: true }));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
-app.use("/auth", require("./routes/authRoute"));
-app.use("/users", require("./routes/userRoute"));
-app.use("/hotels", require("./routes/hotelRoute"));
+app.use("/api/auth", require("./routes/authRoute"));
+app.use("/api/users", require("./routes/userRoute"));
+app.use("/api/hotels", require("./routes/hotelRoute"));
 // app.use("/api/rooms", require("./routes/roomsRoute"));
 
 app.use(errorHandler);
